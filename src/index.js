160c22d95ae07396a324c21e95af2ed1361b202a
henriquebcustodio <77708400+henriquebcustodio@users.noreply.github.com>
2021-08-14 19:00:33 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/products');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MONGO Connection Open');
+    })
+    .catch(err => {
+        console.log('MONGO Connection Error');
+        console.log(err);
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+// Index page
+app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
+});
+
+// New product form
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories });
+});
+
+// Product creation
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+});
+
+// Details page
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/details', { product });
+});
+
+// Edit form
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories });
+});
+
+// Edit product
+app.put('/products/:id/', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+});
+
+app.delete('/products/:id/', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+});
